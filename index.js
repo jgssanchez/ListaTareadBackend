fc87aeb787fdcf74565aa3015b16099be2c952cb
Julio Sanchez <jgs.sanchez94@gmail.com>
2024-03-22 17:11:21 -0300

    agregue el puerto

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import  express  from "express"
 
-console.log("hola mundo")
+const app =express();
+app.set("port", process.env.PORT || 4002);
+app.listen(app.get("port"), ()=>{
+    console.log("Servidor corriendo en el puerto: "+app.get("port"));
+});
